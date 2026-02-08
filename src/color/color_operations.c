29eb582df4ed2a5412d1d74c0ee050a2980348f5
Talen400 <talengithub0@gmail.com>
2026-02-08 01:45:20 -0300

    fix: removing if's to uint32_t

--- src/color/color_operations.c
+++ src/color/color_operations.c
@@ -20,14 +20,6 @@ t_color	color_add(t_color a, t_color b)
 	result.g = a.g + b.g;
 	result.b = a.b + b.b;
 	result.a = a.a + b.a;
-	if (result.r > 255)
-		result.r = 255;
-	if (result.g > 255)
-		result.g = 255;
-	if (result.b > 255)
-		result.b = 255;
-	if (result.a > 255)
-		result.a = 255;
 	return (result);
 }
 
@@ -39,14 +31,6 @@ t_color	color_subtract(t_color a, t_color b)
 	result.g = a.g - b.g;
 	result.b = a.b - b.b;
 	result.a = a.a - b.a;
-	if (result.r < 0)
-		result.r = 0;
-	if (result.g < 0)
-		result.g = 0;
-	if (result.b < 0)
-		result.b = 0;
-	if (result.a < 0)
-		result.a = 0;
 	return (result);
 }
 
@@ -58,13 +42,5 @@ t_color	color_scale(t_color c, double t)
 	result.g = (uint8_t)(c.g * t);
 	result.b = (uint8_t)(c.b * t);
 	result.a = c.a;
-	if (result.r > 255)
-		result.r = 255;
-	if (result.g > 255)
-		result.g = 255;
-	if (result.b > 255)
-		result.b = 255;
-	if (result.a > 255)
-		result.a = 255;
 	return (result);
 }
